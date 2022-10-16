2e0f1bf9d516d6d8480ec015d94fbe030996fada
千夜 <75986733+lyboy-wp@users.noreply.github.com>
2022-10-16 19:10:53 +0800

    Upload by PicGo

---
+++ js/dtimer.js
@@ -0,0 +1,283 @@
+digit =
+    [
+        [
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 1, 1, 0, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 0, 1, 1, 0],
+            [0, 0, 1, 1, 1, 0, 0]
+        ],//0
+        [
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1]
+        ],//1
+        [
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1]
+        ],//2
+        [
+            [1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 1, 1, 0]
+        ],//3
+        [
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 1, 0],
+            [0, 0, 1, 1, 1, 1, 0],
+            [0, 1, 1, 0, 1, 1, 0],
+            [1, 1, 0, 0, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 1, 1]
+        ],//4
+        [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 1, 1, 0]
+        ],//5
+        [
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0],
+            [1, 1, 0, 1, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 1, 1, 0]
+        ],//6
+        [
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0]
+        ],//7
+        [
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 1, 1, 0]
+        ],//8
+        [
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1],
+            [0, 1, 1, 1, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0]
+        ],//9
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]//:10
+    ];
+var WINDOW_WIDTH, WINDOW_HEIGHT, RADIUS, MARGIN_TOP, MARGIN_LEFT;
+var curShowTimeSeconds = 0;
+var balls = [];
+var themeColor = _themeColor;
+var colors = ['#ffcc00', '#ff00cc', '#ccff00', '#cc00ff', '#00ffcc', '#00ccff', '#5a5ea8'];
+$(document).ready(function () {
+    WINDOW_WIDTH = document.body.clientWidth;
+    WINDOW_HEIGHT = window.innerHeight;
+    MARGIN_LEFT = Math.round(WINDOW_WIDTH / 10);
+    MARGIN_TOP = Math.round(WINDOW_HEIGHT / 5);
+    RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108) - 1;
+    var canvas = document.getElementById('corepress-time');
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+    var context = canvas.getContext('2d');
+    curShowTimeSeconds = time_lizijmp_getCurrentShowTimeSeconds();
+    setInterval(function () {
+        time_lizijmp_render(context);
+        time_lizijmp_update()
+
+    }, 50)
+});
+
+function time_lizijmp_update() {
+    var nextShowTimeSeconds = time_lizijmp_getCurrentShowTimeSeconds();
+    var nextHour = parseInt(nextShowTimeSeconds / 3600);
+    var nextMinute = parseInt((nextShowTimeSeconds - nextHour * 3600) / 60);
+    var nextSecond = nextShowTimeSeconds % 60;
+    var curHour = parseInt(curShowTimeSeconds / 3600);
+    var curMinute = parseInt((curShowTimeSeconds - curHour * 3600) / 60);
+    var curSecond = curShowTimeSeconds % 60;
+    if (nextSecond != curSecond) {
+
+        if (parseInt(curHour / 10) != parseInt(nextHour / 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT, MARGIN_TOP, parseInt(curHour / 10));
+        }
+
+        if (parseInt(curHour % 10) != parseInt(nextHour % 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(curHour % 10));
+        }
+
+        if (parseInt(curMinute / 10) != parseInt(nextMinute / 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinute / 10));
+        }
+
+        if (parseInt(curMinute % 10) != parseInt(nextMinute % 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinute % 10));
+        }
+        if (parseInt(curSecond / 10) != parseInt(nextSecond / 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT + 79 * (RADIUS + 1), MARGIN_TOP, parseInt(curSecond / 10));
+        }
+        if (parseInt(curSecond % 10) != parseInt(nextSecond % 10)) {
+            time_lizijmp_addBalls(MARGIN_LEFT + 94 * (RADIUS + 1), MARGIN_TOP, parseInt(nextSecond % 10));
+        }
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+    time_lizijmp_updateBalls();
+}
+
+function time_lizijmp_updateBalls() {
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        if (balls[i].y >= WINDOW_HEIGHT - RADIUS) {
+            balls[i].y = WINDOW_HEIGHT - RADIUS;
+            balls[i].vy = -balls[i].vy * 0.75;
+        }
+    }
+
+    var count = 0;
+    for (var i = 0; i < balls.length; i++) {
+        if (balls[i].x + RADIUS > 0 && balls[i].x - RADIUS < WINDOW_WIDTH) {
+            balls[count++] = balls[i]
+        }
+    }
+    while (balls.length > count) {
+        balls.pop();
+    }
+}
+
+function time_lizijmp_addBalls(x, y, num) {
+    for (var i = 0; i < digit[num].length; i++) {
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if (digit[num][i][j] == 1) {
+                var aBall = {
+                    x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    g: 1.5 + Math.random(),
+                    vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,//鍙�4鎴栬€�-4
+                    vy: -5,
+                    color: colors[Math.floor(Math.random() * colors.length)]
+                }
+                balls.push(aBall);
+            }
+        }
+    }
+}
+
+function time_lizijmp_getCurrentShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = curTime.getHours() * 3600 + curTime.getMinutes() * 60 + curTime.getSeconds();
+    return ret;
+}
+
+function time_lizijmp_render(ctx) {
+    ctx.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+    var hour = parseInt(curShowTimeSeconds / 3600);
+    var minute = parseInt((curShowTimeSeconds - hour * 3600) / 60);
+    var second = curShowTimeSeconds % 60;
+
+    time_lizijmp_renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hour / 10), ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hour % 10), ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minute / 10), ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minute % 10), ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 79 * (RADIUS + 1), MARGIN_TOP, parseInt(second / 10), ctx);
+    time_lizijmp_renderDigit(MARGIN_LEFT + 94 * (RADIUS + 1), MARGIN_TOP, parseInt(second % 10), ctx);
+    for (var i = 0; i < balls.length; i++) {
+        ctx.fillStyle = balls[i].color;
+        ctx.beginPath();
+        ctx.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+}
+
+function time_lizijmp_renderDigit(x, y, num, ctx) {
+    for (var i = 0; i < digit[num].length; i++) {
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if (digit[num][i][j] == 1) {
+                time_lizijmp_draw(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 * (RADIUS + 1) + (RADIUS + 1), RADIUS, ctx)
+            }
+        }
+    }
+}
+
+function time_lizijmp_draw(x, y, r, ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = themeColor;
+    ctx.fill();
+    ctx.closePath();
+}
